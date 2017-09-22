e584812eb1ac13ec1069d910506767953e5269e9
etesta6 <etesta6@gatech.edu>
2017-09-22 23:59:13 +0200

    populate refactored

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -40,86 +40,9 @@ exports.getUser = function(req, res) {
     })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 exports.populate = function(req, res) {
-
-    var user1 = new User({
-        firstName: "Enzo",
-        lastName: "Testa",
-        email: "enzotesta@hotmail.fr",
-        phone: "0674747372",
-        sex: "M",
-        move: "yes",
-        city: "Paris",
-      
-        teacher: 0,
-        student: 0,
-      
-        shortDescription: "Un bg",
-        longDescription: "Mais vriament très$&é§§ bien",
-        grade: "Computer Science",
-      
-        lessons: new Array() 
-    })
-
-
-    var user2 = new User({
-        firstName: "Alexis",
-        lastName: "Lozano",
-        email: "alexisno@hotmail.fr",
-        phone: "0674347372",
-        sex: "M",
-        move: "yes",
-        city: "Paris",
-      
-        teacher: 0,
-        student: 0,
-      
-        shortDescription: "Un bg",
-        longDescription: "Mais vriament très$&é§§ bien",
-        grade: "Computer Science",
-      
-        lessons: new Array() 
-    })
-
-    user1.save()
-    .then(item => {
-        user2.save()
-        .then(item => {
-            res.send("Alexis and Enzo saved");
-        })
-        .catch(err => {
-            res.status(400).send("Alexis nope")
-        })
-    })
-    .catch(err => {
-        res.status(400).send("Enzo nope")
+    var users = require('../data/populateUsers')
+    users.map( user => {
+        new User({user}).save();
     })
 }
