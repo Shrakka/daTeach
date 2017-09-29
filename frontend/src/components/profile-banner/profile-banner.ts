143df08f083e336db60cc13fa3291c3289d9b97e
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2017-09-29 22:25:42 +0200

    ProfileBanner finished

--- frontend/src/components/profile-banner/profile-banner.ts
+++ frontend/src/components/profile-banner/profile-banner.ts
@@ -1,22 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-/**
- * Generated class for the ProfileBannerComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
   selector: 'profile-banner',
   templateUrl: 'profile-banner.html'
 })
-export class ProfileBannerComponent {
 
-  text: string;
+export class ProfileBannerComponent {
+  @Input() firstname: string;
+  @Input() lastname: string;
+  @Input() picture: string;
+  @Input() shortDescription: string;
 
   constructor() {
-    console.log('Hello ProfileBannerComponent Component');
-    this.text = 'Hello World';
   }
 
 }
--- frontend/src/pages/profile/profile.module.ts
+++ frontend/src/pages/profile/profile.module.ts
@@ -3,12 +3,14 @@ import { IonicPageModule } from 'ionic-angular';
 import { ProfilePage } from './profile';
 import { HeaderComponentModule } from '../../components/header/header.module';
 import { FooterComponentModule } from '../../components/footer/footer.module';
+import { ProfileBannerComponentModule } from '../../components/profile-banner/profile-banner.module'
 
 @NgModule({
   declarations: [
     ProfilePage,
   ],
   imports: [
+    ProfileBannerComponentModule,
     FooterComponentModule,
     HeaderComponentModule,
     IonicPageModule.forChild(ProfilePage),
