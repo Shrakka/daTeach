9e9c0b0538ad7b2272d4337f1efbc724e25cc94a
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2017-10-09 13:32:11 +0200

    Backend discussion - small edit

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose'),
 
 module.exports = function(io) {
   io.on('connection', (socket) => {
-    socket.on('addMessage', (content, discussion, author) => {
+    socket.on('addMessage', (value) => {
       var time = new Date()
-      io.emit('message', {content: content, discussion: discussion, author: author, time: time});
+      io.emit('message', {content: value.content, discussion: value.discussion, author: value.author, time: time});
       Discussion.findByIdAndUpdate (
-        discussion,
-        {$push: {"messages": {time: time, author: author, content: content}}},
+        value.discussion,
+        {$push: {"messages": {time: time, author: value.author, content: value.content}}},
         {safe: true, upsert: true, new : true},
         function(err, model) {
             console.log(err);
