d6145ab1aaa6cd761effce3caf0e06c1c6f2472a
etesta6 <etesta6@gatech.edu>
2017-10-21 19:07:18 +0200

    details modifed

--- frontend/src/pages/detail/detail.ts
+++ frontend/src/pages/detail/detail.ts
@@ -16,8 +16,8 @@ export class DetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public lessonProvider: LessonProvider, public discussionProvider: DiscussionProvider, public userProvider: UserProvider) {
     this.comment = "";
-    this.placeholder = "";
     this.result = this.navParams.get('result');
+    this.placeholder = this.result.user.public.askmessage ? 'Write a short message to ' + this.result.user.public.firstname+ ' to present you and explain what you are looking for' : 'Hi '+ this.result.user.public.firstname + ', I am interesting in your announce';
   }
 
   ionViewDidLoad() {
@@ -28,16 +28,13 @@ export class DetailPage {
     this.lessonProvider.addPeople({people: this.userProvider.user.id, role: (this.result.lesson.teachers.includes(this.result.user.id)) ? 'student' : 'teacher'}, this.result.lesson._id)
     const alert = this.alertCtrl.create({
       title: 'Message sent',
-      message: this.result.user.public.firstname + ' will certainly contact you soon. You will be notified.',
+      message: 'Your message has been sent successfully to ' + this.result.user.public.firstname + '. He will certainly contact you soon and you will be notified.',
       buttons: [
         {
           text: 'OK',
           role: 'cancel',
           handler: () => {
-            this.comment = "";
-            this.placeholder = "You can still send 1 more message to " + this.result.user.public.firstname;
-
-            console.log('OK clicked');
+            this.navCtrl.pop();
           }
         }],
     })
