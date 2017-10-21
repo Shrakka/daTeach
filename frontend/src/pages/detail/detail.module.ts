c59fa19307bc9b01caa1e76fa375f57d04612092
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2017-10-21 16:42:43 +0200

    Clandar added to Detail page

--- frontend/src/pages/detail/detail.module.ts
+++ frontend/src/pages/detail/detail.module.ts
@@ -3,12 +3,14 @@ import { IonicPageModule } from 'ionic-angular';
 import { DetailPage } from './detail';
 import { HeaderComponentModule } from '../../components/header/header.module';
 import { FooterComponentModule } from '../../components/footer/footer.module';
+import { CalendarComponentModule } from '../../components/calendar/calendar.module';
 
 @NgModule({
   declarations: [
     DetailPage,
   ],
   imports: [
+    CalendarComponentModule,
     HeaderComponentModule,
     FooterComponentModule,
     IonicPageModule.forChild(DetailPage),
