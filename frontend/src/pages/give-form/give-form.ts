9c5795affa8218b79f20b7d1cfe6d1dce900a002
etesta6 <etesta6@gatech.edu>
2017-10-21 12:56:09 +0200

    done give-form + started matching algorithm

--- frontend/src/pages/give-form/give-form.ts
+++ frontend/src/pages/give-form/give-form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, AlertController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
 import { LessonProvider } from '../../providers/lesson/lesson';
 import { TopicModalPage } from '../../pages/topic-modal/topic-modal';
@@ -12,66 +12,67 @@ import { LocationModalPage } from '../../pages/location-modal/location-modal';
 })
 export class GiveFormPage {
 
-  formValue = {
-    role: 'teacher',
-    type: 'regular',
-    moving: 'move',
-    topics: [],
-    location: '',
-    dates: []
-  };
+  giveForm: any;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider: UserProvider, public modalCtrl: ModalController, public lessonProvider: LessonProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider: UserProvider, public modalCtrl: ModalController, public lessonProvider: LessonProvider, public alertCtrl: AlertController) {
+    this.giveForm =   {
+      role: 'teacher',
+      type: 'regular',
+      moving: 'move',
+      topics: [],
+      location: '',
+      dates: []
+      };
+
+    this.lessonProvider.getTopics();
+  
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GiveFormPage');
+  ionViewDidLoad() {console.log('ionViewDidLoad GiveFormPage');}
+  selectedRegular() {}
+  selectedPunctual() {}
+
+  setMoving(moving){
+    this.giveForm.moving = moving;
+    console.log(this.giveForm);
   }
-
-  goToResults() {
-    this.lessonProvider.postLessonRequest(this.formValue);
-    this.navCtrl.push('ResultsPage', {mode: "give"});
-  }
-
-  selectedRegular() {
-
-  }
-
-  selectedPunctual() {
-
-  }
-
-  searchTopic($event){
-    console.log($event);
-  }
-
-  searchLocation($event){
-    console.log($event);
-  }
-
+  
   onTopicFocus(){
     const topicModal = this.modalCtrl.create(TopicModalPage, {'give':true});
 
     topicModal.onDidDismiss(data => {
-      this.formValue.topics = data;
-      console.log(this.formValue.topics);
+      this.giveForm.topics = data;
+      console.log(this.giveForm.topics);
     })
     topicModal.present();
   }
 
   onLocationFocus(){
     const locationModal = this.modalCtrl.create(LocationModalPage, {'give':true});
-
     locationModal.onDidDismiss(data => {
-      this.formValue.location = data.name;
+      this.giveForm.location = data.name;
     })
     locationModal.present();
   }
 
   onDatesSelection($event) {
     // EVENT = LIST DE DATE
-    this.formValue.dates = $event;
+    this.giveForm.dates = $event;
+  }
+
+  goToResults() {
+    if(this.giveForm.location === '' || this.giveForm.topics === []){
+      const alert = this.alertCtrl.create({
+        title:'Complete the form',
+        subTitle:'Please fill the empty fields to continue',
+        buttons: ['OK']
+      });
+      alert.present();
+    } else {
+      this.lessonProvider.postLessonRequest(this.giveForm);
+      this.navCtrl.push('ResultsPage', {mode: "give"});
+    }
   }
 
 }
