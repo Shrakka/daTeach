840ce6e584797122cdf12b3878818db6eef73eae
Arnaud YANKWA WANDJI <yankwarmand@gmail.com>
2017-10-23 11:36:28 +0200

    get latlong coordinates

--- frontend/src/pages/location-modal/location-modal.ts
+++ frontend/src/pages/location-modal/location-modal.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 @IonicPage()
@@ -24,7 +26,8 @@ export class LocationModalPage {
   locations = [];
   give: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public viewCtrl: ViewController, private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) {
     this.give = navParams.get('give');
     
     for(let l of this.RawLocations){
@@ -52,6 +55,24 @@ export class LocationModalPage {
     console.log(location);
     this.pickedLocation = location;
     this.closeModal();
+  }
+
+  getposition() {
+    this.geolocation.getCurrentPosition()
+      .then((resp) => {
+       console.log('latitude', resp.coords.latitude);
+       console.log('longitude', resp.coords.longitude);
+       alert(resp.coords.latitude + ", " + resp.coords.longitude);
+
+       this.nativeGeocoder.reverseGeocode(resp.coords.latitude, resp.coords.longitude)
+      .then((result: NativeGeocoderReverseResult) => console.log(JSON.stringify(result)))
+      .catch((error: any) => console.log(error));
+      
+     })
+      .catch((error) => {
+    console.log('Error getting location', error);});
+
+    
   }
 
 }
