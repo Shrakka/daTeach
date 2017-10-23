0615e550ea346802e5ff67b78c30bc68cc4e74e2
Arnaud YANKWA WANDJI <yankwarmand@gmail.com>
2017-10-23 14:25:52 +0200

    location modal retrieve full address text!

--- frontend/src/pages/location-modal/location-modal.ts
+++ frontend/src/pages/location-modal/location-modal.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation';
+import { Http } from '@angular/http';
 import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
@@ -26,7 +27,7 @@ export class LocationModalPage {
   locations = [];
   give: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, 
+  constructor(public navCtrl: NavController, public navParams: NavParams,  public http: Http,
     public viewCtrl: ViewController, private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) {
     this.give = navParams.get('give');
     
@@ -62,11 +63,20 @@ export class LocationModalPage {
       .then((resp) => {
        console.log('latitude', resp.coords.latitude);
        console.log('longitude', resp.coords.longitude);
-       alert(resp.coords.latitude + ", " + resp.coords.longitude);
+       // alert(resp.coords.latitude + ", " + resp.coords.longitude);
 
-       this.nativeGeocoder.reverseGeocode(resp.coords.latitude, resp.coords.longitude)
-      .then((result: NativeGeocoderReverseResult) => console.log(JSON.stringify(result)))
-      .catch((error: any) => console.log(error));
+       var requestUrl="https://maps.googleapis.com/maps/api/geocode/json?latlng="+resp.coords.latitude+","+resp.coords.longitude+
+       "&key=AIzaSyCvYUJBCSnda6uaadmkzlRtDIeWE7QSPlU";
+
+       this.http.get(requestUrl).map(res => res.json()).subscribe(data => {
+        this.pickedLocation={name: data.results[0].formatted_address, clicked:false};
+        this.closeModal();
+        });
+
+
+      //  this.nativeGeocoder.reverseGeocode(resp.coords.latitude, resp.coords.longitude)
+      // .then((result: NativeGeocoderReverseResult) => console.log(JSON.stringify(result)))
+      // .catch((error: any) => console.log(error));
       
      })
       .catch((error) => {
