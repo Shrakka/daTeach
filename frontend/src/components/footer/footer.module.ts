f6d14469b265b87856e54f070fcbd904ae5ab965
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2017-11-13 22:05:49 +0100

    Footer buttons strings

--- frontend/src/components/footer/footer.module.ts
+++ frontend/src/components/footer/footer.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer';
 import { IonicPageModule } from 'ionic-angular';
+import { TranslateModule } from 'ng2-translate/ng2-translate';
 
 @NgModule({
 	declarations: [FooterComponent],
-	imports: [IonicPageModule.forChild(FooterComponent)],
+	imports: [
+		IonicPageModule.forChild(FooterComponent),
+		TranslateModule
+	],
 	exports: [FooterComponent]
 })
 
