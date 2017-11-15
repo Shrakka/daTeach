b250df8b1c33b7591d7aac04a9bf7e708e295717
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2017-11-15 13:52:36 +0100

    amazon urls

--- frontend/src/providers/backend/backend.ts
+++ frontend/src/providers/backend/backend.ts
@@ -10,8 +10,8 @@ export class BackendProvider {
 
   constructor(public http: Http) {
     console.log('Hello BackendProvider Provider');
-    //this.url = 'http://ec2-54-77-151-59.eu-west-1.compute.amazonaws.com:8080';
-    this.url = 'http://localhost:8080';
+    this.url = 'http://ec2-54-77-151-59.eu-west-1.compute.amazonaws.com:8080';
+    // this.url = 'http://localhost:8080';
     // this.url = 'http://10.42.0.1:8080';
     // this.url = 'http://192.168.2.1:8080'
     this.apikey = 'VxWSMnBRcTTGXMFRzAoAcyFQLDYZ8pQW';
@@ -19,5 +19,6 @@ export class BackendProvider {
   }
 }
 
-//export const socketURL = 'http://10.42.0.1:8080';
-export const socketURL = 'http://192.168.2.1:8080';
+// export const socketURL = 'http://10.42.0.1:8080';
+// export const socketURL = 'http://192.168.2.1:8080';
+export const socketURL = 'http://ec2-54-77-151-59.eu-west-1.compute.amazonaws.com:8080';
