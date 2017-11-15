61e5af16740bfedbf5abf8357332d8426710dce6
etesta6 <etesta6@gatech.edu>
2017-11-15 14:41:06 +0100

    some update

--- frontend/src/components/result-map/result-map.ts
+++ frontend/src/components/result-map/result-map.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NavController, NavParams  } from 'ionic-angular';
 import { LessonProvider } from '../../providers/lesson/lesson';
+import { UserProvider } from '../../providers/user/user';
 
 @Component({
   selector: 'result-map',
@@ -10,7 +11,7 @@ export class ResultMapComponent {
   @Input() result: any;
   @Input() mode: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public lessonProvider: LessonProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public lessonProvider: LessonProvider, public userProvider: UserProvider) {
 
     if(navParams.data.result){//Access from the map
     this.result = navParams.data.result;
